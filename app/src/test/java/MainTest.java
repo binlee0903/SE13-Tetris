51c6efd02f6e50933341308265f41b385638dc3a
someh <someh2705@gmail.com>
2024-03-16 17:46:20 +0900

    feat: setting up gradle test environment with Github Actions

---
+++ app/src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testing() {
+        assertEquals(1, 1);
+    }
+}
